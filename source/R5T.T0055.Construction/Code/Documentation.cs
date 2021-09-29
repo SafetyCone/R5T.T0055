b1af245c8986b26aa9e95c09b7a03182207c989d
davidcoats <david@getsafetycone.com>
2021-09-28 21:35:53 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0055.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0055.Construction
+{
+    /// <summary>
+    /// Construction project for IGuidOperator and IGuid extension method base types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0055/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0055
+{
+    /// <summary>
+    /// IGuidOperator and IGuid extension method base types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
