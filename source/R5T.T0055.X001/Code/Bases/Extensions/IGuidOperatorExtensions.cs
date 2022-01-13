5d51b677c5472086ac5dbceadb9d11ab76143e17
David Coats <david@getsafetycone.com>
2022-01-13 00:20:31 -0800

    Add extensions for reading guids from text files.

--- source/R5T.T0055.X001/Code/Bases/Extensions/IGuidOperatorExtensions.cs
+++ source/R5T.T0055.X001/Code/Bases/Extensions/IGuidOperatorExtensions.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 using R5T.T0055;
 
@@ -69,6 +72,74 @@ namespace System
 
             var output = new Guid(guidBytes);
             return output;
+        }
+    }
+}
+
+
+namespace System.IO
+{
+    using R5T.Magyar.IO;
+
+
+    public static partial class IGuidOperatorExtensions
+    {
+        public static async Task<Guid[]> ReadGuidsFromTextFile(this IGuidOperator _,
+            TextReader reader,
+            string separator,
+            Func<string, Guid> parser)
+        {
+            var data = await reader.ReadToEndAsync();
+
+            // Do not keep empty tokens.
+            var tokens = data.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            var output = tokens
+                .Select(parser)
+                .Now();
+
+            return output;
+        }
+
+        public static async Task<Guid[]> ReadGuidsFromTextFile(this IGuidOperator _,
+            string filePath)
+        {
+            using var reader = TextReaderHelper.New(filePath);
+
+            var output = await _.ReadGuidsFromTextFile(
+                reader,
+                Strings.NewLineForEnvironment,
+                _.FromStringStandard);
+
+            return output;
+        }
+
+        public static async Task WriteGuidsToTextFile(this IGuidOperator _,
+            TextWriter writer,
+            IEnumerable<Guid> values,
+            string separator,
+            Func<Guid, string> formatter)
+        {
+            var tokens = values
+                .Select(formatter)
+                .Now();
+
+            var data = String.Join(separator, tokens);
+
+            await writer.WriteAsync(data);
+        }
+
+        public static async Task WriteGuidsToTextFile(this IGuidOperator _,
+            string filePath,
+            IEnumerable<Guid> values)
+        {
+            using var writer = TextWriterHelper.New(filePath);
+
+            await _.WriteGuidsToTextFile(
+                writer,
+                values,
+                Strings.NewLineForEnvironment,
+                _.ToStringStandard);
         }
     }
 }
